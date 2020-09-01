989b6f1049070f50d1fddce3132409d4371fcb67
Katie Cannon <39453428+KatieCannon@users.noreply.github.com>
2020-09-01 10:46:01 +0100

    adding test to get last number in array

--- CSharpKatas/CSharpKatasTests/ArrayKatasTests.cs
+++ CSharpKatas/CSharpKatasTests/ArrayKatasTests.cs
@@ -14,9 +14,19 @@ namespace CSharpKatasTests
         {
             string[] nameArray = { "apple", "orange", "banana" };
 
-            string result = ArrayKatas.getLastNameInArray(nameArray);
+            string result = ArrayKatas.getLastItemInArray(nameArray);
 
             Assert.AreEqual("banana", result);
+        }
+
+        [TestMethod]
+        public void getLastNumberInArray()
+        {
+            int[] numberArray = { 1, 2, 3 };
+
+            int result = ArrayKatas.getLastItemInArray(numberArray);
+
+            Assert.AreEqual(3, result);
         }
     }
 }
