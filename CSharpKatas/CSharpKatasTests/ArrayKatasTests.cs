a57cc5b3d24dec1c9ddf3d4c22cac75424776bfd
Katie Cannon <39453428+KatieCannon@users.noreply.github.com>
2020-09-01 15:30:18 +0100

    test for getting last item in array dynamically

--- CSharpKatas/CSharpKatasTests/ArrayKatasTests.cs
+++ CSharpKatas/CSharpKatasTests/ArrayKatasTests.cs
@@ -8,23 +8,46 @@ namespace CSharpKatasTests
     [TestClass]
     public class ArrayKatasTests
     {
-        
-        [TestMethod]
-        public void getLastNameInArray()
-        {
-            string[] nameArray = { "apple", "orange", "banana" };
-
-            string result = ArrayKatas.getLastItemInArray(nameArray);
-
-            Assert.AreEqual("banana", result);
-        }
-
         [TestMethod]
         public void getLastNumberInArray()
         {
             int[] numberArray = { 1, 2, 3 };
 
-            int result = ArrayKatas.getLastItemInArray(numberArray);
+            int result = ArrayKatas.getLastItemInArrayByOverloading(numberArray);
+
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void getLastNameInArray()
+        {
+            string[] nameArray = { "apple", "orange", "banana" };
+
+            string result = ArrayKatas.getLastItemInArrayByOverloading(nameArray);
+
+            Assert.AreEqual("banana", result);
+        }
+
+        [TestMethod]
+        public void getLastNameItemInArray()
+        {
+            ArrayKatas arrayKata = new ArrayKatas();
+
+            string[] nameArray = { "apple", "orange", "banana" };
+
+            string result = arrayKata.getLastItemInArray(nameArray);
+
+            Assert.AreEqual("banana", result);
+        }
+
+        [TestMethod]
+        public void getLastNumberItemInArray()
+        {
+            ArrayKatas arrayKata = new ArrayKatas();
+
+            int[] numberArray = { 1, 2, 3 };
+
+            int result = arrayKata.getLastItemInArray(numberArray);
 
             Assert.AreEqual(3, result);
         }
