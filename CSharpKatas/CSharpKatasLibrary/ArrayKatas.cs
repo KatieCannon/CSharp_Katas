45fb035090f01a13d3551707c287d93b25d8a9bc
Katie Cannon <39453428+KatieCannon@users.noreply.github.com>
2020-09-01 10:18:35 +0100

    initial setup of 1st kata and tests

---
+++ CSharpKatas/CSharpKatasLibrary/ArrayKatas.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace CSharpKatasLibrary
+{
+    public class ArrayKatas
+    {
+        public static string getLastNameInArray(string[] nameArray)
+        {
+            return nameArray[nameArray.Length -1];
+        }
+    }
+}
---
+++ CSharpKatas/CSharpKatasTests/ArrayKatasTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using CSharpKatasLibrary;
+
+namespace CSharpKatasTests
+{
+    
+    [TestClass]
+    public class ArrayKatasTests
+    {
+        
+        [TestMethod]
+        public void getLastNameInArray()
+        {
+            string[] nameArray = { "apple", "orange", "banana" };
+
+            string result = ArrayKatas.getLastNameInArray(nameArray);
+
+            Assert.AreEqual("banana", result);
+        }
+    }
+}
+
